3038142fed6495e8f80efe00d6a3de29ff7b4acc
subhadeepdhar17 <oliverdepp17@gmail.com>
2023-09-09 15:35:59 +0530

    Firebase connected

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Navbar from './Components/Navbar';
+import { AuthContextProvider } from './Context/AuthContext';
+import Account from './Pages/Account';
 import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+    <AuthContextProvider>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
+    </AuthContextProvider>
     </>
   );
 }
---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <>
+    <div className='w-full h-screen'>
+        <img className='sm:block absolute w-full h-full object-cover' src='https://assets.nflxext.com/ffe/siteui/vlv3/42df4e1f-bef6-499e-87ff-c990584de314/5e7c383c-1f88-4983-b4da-06e14c0984ba/IN-en-20230904-popsignuptwoweeks-perspective_alpha_website_small.jpg' alt='/' />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'>
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Signup
