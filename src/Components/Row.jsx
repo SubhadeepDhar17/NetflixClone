2c3b82c42c8a7fbc855f85f7d945b8cbc7c74d5c
subhadeepdhar17 <oliverdepp17@gmail.com>
2023-09-09 10:48:35 +0530

    Added Movies

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Movie from './Movie'
 
 const Row = ({title, fetchURL}) => {
     const [movies, setMovies] = useState([])
@@ -9,16 +10,15 @@ const Row = ({title, fetchURL}) => {
             setMovies(response.data.results)
         })
     },[fetchURL])
-console.log(movies)
+
   return (
     <>
     <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-    <div className='relative flex items-center'>
-        <div id={'slider'}>
+    <div className='relative flex items-center group'>
+        <div id={'slider'}
+            className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
             {movies.map((item, id) => (
-                <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                    <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.title} />
-                </div>
+                <Movie key={id} item={item}/>
             ))}
         </div>
     </div>
@@ -27,3 +27,22 @@ console.log(movies)
 }
 
 export default Row
+
+/*const slideLeft = () => {
+        var slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft - 500
+    }
+
+    const slideRight = () => {
+        var slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft + 500
+    }*/
+
+/*<MdChevronLeft
+            onClick={slideLeft}
+            className='bg-white left-2 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' 
+            size={40}/>
+<MdChevronRight
+            onClick={slideRight}
+            className='bg-white right-2 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' 
+            size={40}/>*/
